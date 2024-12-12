8d0f790e744d836b03f5b97d7b92d82bb2299e2a
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2024-12-12 04:19:12 +0100

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -20,6 +20,7 @@ df = pd.concat([train, test], ignore_index=True).reset_index(drop=True)
 ########### PREPROCESS DATA ####################
 ################################################
 
+print("Initializing Data Processing.")
 df['duration'] = (df['duration']/7).round(0).astype('int64')
 # df['duration'] = df['duration'].astype('category')
 
@@ -36,6 +37,7 @@ for cat in cats:
 todrop = ['disbursement_date', 'due_date', 'country_id', 'tbl_loan_id', 'New_versus_Repeat']
 df = df.drop(todrop, axis=1)
 df['duration'] = df['duration'].astype('int')
+print("Processing Done.")
 
 train = df[df['target'] != -1]
 test = df[df['target'] == -1]
