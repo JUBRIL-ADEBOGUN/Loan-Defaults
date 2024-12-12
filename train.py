1475ca7570f35b389c4f2fba301e3f75a40b390e
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2024-12-12 04:47:01 +0100

    quotation error

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ title_fs = 22 #title font size
 sns.set(style='whitegrid')
 
 
-ax = sns.barplot(data=feature_df, x=features, y=importances)
+ax = sns.barplot(data=feature_df, x='features', y='importances')
 ax.set_xlabel("Importance", fontsize=axis_fs)
 ax.set_ylabelO('Feature', fontsize=axis_fs)
 ax.set_title('LightGBM\n feature importance', fontsize=title_fs)
