caaffb505b5f0e1f4f1382f47422af420e5f317d
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2024-12-13 03:42:06 +0100

    Update print

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ y_train = trainx.target
 X_val = val.drop(todrop, axis=1)
 y_val = val.target
 
-print(f"Features use during training are: {list(X_train.column)}")
+print(f"Features use during training are: {list(X_train.columns)}")
 lgbclf = lgb.LGBMClassifier(random_state=23, force_col_wise=True, max_depth=11, n_estimators=500,
                             importance_type='gain', #early_stopping_rounds=100
                             )
